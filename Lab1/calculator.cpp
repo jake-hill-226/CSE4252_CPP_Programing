ca71fe9de071914c50547931c57927cf45bc06dd
Jake Hill <Jake Hill>
2017-09-21 21:12:19 -0400

    Initial commit. Added source code for Lab1 calculator app.

---
+++ Lab1/calculator.cpp
@@ -0,0 +1,141 @@
+/**
+ * Calculator.cpp
+ * Desc: Simple console based calculator
+ * 		 --For CSE 4252, Lab1
+ * Author: Jake Hill
+ * Last Reviewed: 9/15/17 
+ */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <math.h>
+
+using namespace std;
+
+void printBuffer(int);
+
+int main(){
+
+	double val_1;
+	double val_2;
+	char op;
+	bool valid_input = false;
+
+	cout << "Welcome to EasyCalc" << endl
+		 << "Edumacated Solutions" << endl
+		 << "--Copyright (c) 2017--" << endl;
+
+	printBuffer(2);
+
+	cout << "Lets get started!" << endl;
+	
+	// Application Loop
+	do{
+		cout << "Choose your op," << endl
+			<< "# + : addition" << endl
+			<< "# - : subtraction" << endl
+			<< "# * : multiplication" << endl
+			<< "# / : division" << endl
+			<< "# % : modulous" << endl
+			<< "(choose 'q' to quit)" << endl;
+		
+		// Input Loop
+		while(!valid_input){
+			cout << "Choice? : ";
+			cin >> op;
+
+			switch(op){
+				case '+' : valid_input = true;
+							break;
+				case '-' : valid_input = true;
+							break;
+				case '*' : valid_input = true;
+							break;
+				case '/' : valid_input = true;
+							break;
+				case '%' : valid_input = true;
+							break;
+				case 'q' : valid_input = true;
+							break;
+				default : cout << op << " is not a valid operator, try again"
+								<< endl << endl;
+			}
+
+			//Clear out the buffer before accessing the stream again
+			cin.clear();
+			fflush(stdin);
+		}
+
+		valid_input = false;
+
+		if(op != 'q'){
+			cout << "Enter first operand: ";
+			cin >> val_1;
+			//Clear out the buffer before accessing the stream again
+			cin.clear();
+			fflush(stdin);
+			
+			cout << "Enter second operand: ";
+			cin >> val_2;
+			
+			//Clear out the buffer before accessing the stream again
+			cin.clear();
+			fflush(stdin);
+
+			// Catch Division by zero
+			if(val_2 == 0 && op == '/'){
+				cout << endl << "Cannot divide by 0, try again";
+				printBuffer(4);
+				continue;
+			}
+
+			stringstream result;
+			result << val_1 << " " << op << " " << val_2 << " = ";
+
+			// Perform the operation
+			// Check for overflow cases 
+			switch(op){
+				case '+' : if((val_1 + val_2) > val_1){
+								result << (val_1 + val_2);
+							}else{
+								result.str("Numbers are too large");
+							}
+							
+							break;
+				case '-' : if((val_1 - val_2) < val_1){
+								result << (val_1 - val_2);
+							}else{
+								result.str("Numbers are too large");
+							}
+							break;
+				case '*' : if((val_1 - val_2) > val_1){
+								result << (val_1 - val_2);
+							}else{
+								result.str("Numbers are too large");
+							}
+							break;
+				case '/' : result << (val_1 / val_2);
+							break;
+				case '%' : result << fmod(val_1, val_2);
+							break;
+			}
+
+			cout << endl << result.str();
+
+			printBuffer(4);
+		}
+		
+	}while(op != 'q');
+
+	return 0;
+}
+
+// Prints {num_lines} return characters followed
+// by a horizontal separator to the stdout buffer
+void printBuffer(int num_lines){
+	for(int i = 0; i < num_lines; i++){
+		cout << endl;
+	}
+	cout << "--------------------------------------" << endl;
+}
